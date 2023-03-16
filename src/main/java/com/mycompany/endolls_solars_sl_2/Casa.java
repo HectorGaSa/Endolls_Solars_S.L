67ee178475d5af4fa7162a95d9ec8f450d0590f3
Hector Garcia Sala <hectorgarciasala20@gmail.com>
2023-03-16 17:12:07 +0100

    Solución de errores, comentarios y implementacion de nuevos metodos en archivo Casa.java

--- src/main/java/com/mycompany/endolls_solars_sl_2/Casa.java
+++ src/main/java/com/mycompany/endolls_solars_sl_2/Casa.java
@@ -18,8 +18,8 @@ public class Casa {
     private String nom;
     private int superficie;
     private boolean interruptor;
-    static ArrayList<Placa> placas = new ArrayList();
-    static ArrayList<Aparell> aparells = new ArrayList();
+    private ArrayList<Placa> placas = new ArrayList();
+    private ArrayList<Aparell> aparells = new ArrayList();
 
     //Constructor de la clase "Casa"
     public Casa(String NIF, String nom, int superficie) {
@@ -44,88 +44,109 @@ public class Casa {
     public boolean getInterruptor() {
         return interruptor;
     }
-    
-    //Funciones para poder añadir una placa y un aparato a la casa
+//Metodo para poder utilizar los datos de la ArrayList de placas y asi tanto en info como en list conseguir mostrar cuantas placas tenemos en la casa.
+    public ArrayList getPlacas() {
+        return placas;
+    }
+//Metodo para poder utilizar los datos de la ArrayList de aparells y asi tanto en info como en list conseguir mostrar cuantas aparatos tenemos en la casa.
+    public ArrayList getAparells() {
+        return aparells;
+    }
+
+    //Con este metodo reviso que aparatos estan encendidos y si es asi me imprimirá su descripcion en la info de su respectiva casa.
+    public void getAparellsEncesos() {
+        for (Aparell i : aparells) {
+            if (i.getInterruptor() == true) {
+                System.out.println("- " + i.getDescripcion());
+            }
+        } 
+    }
+
+    //Funciones para poder añadir una placa y un aparato a la casa.
     public void addplaca(Placa nueva) {
-    placas.add(nueva);
+        placas.add(nueva);
     }
+
     public void addaparell(Aparell nuevo) {
-    aparells.add(nuevo);
+        aparells.add(nuevo);
     }
-    //Funcion para poder encender el interruptor general de la casa que se nos indique
+
+    //Funcion para poder encender el interruptor general de la casa que se nos indique.
     public void oncasa() {
-    if (interruptor == false) {
-        this.interruptor = true;
-    } 
+        if (interruptor == false) {
+            this.interruptor = true;
+        }
     }
-    
+//Funcion para poder apagar el interruptor general de la casa que se nos indique.
     public void offcasa() {
-    if (interruptor == true) {
-        this.interruptor = false;
+        if (interruptor == true) {
+            this.interruptor = false;
+        }
     }
-    }
-    
-    //Funcion para poder buscar el aparato que deseamos
+
+    //Funcion para poder buscar el aparato que deseamos.
     public Aparell buscaraparell(String Descripcion) {
         String buscado = Descripcion;
         for (Aparell i : aparells) {
             if (i != null && i.getDescripcion().equals(buscado)) {
                 return i;
             }
-        } return null;
-    }
-    public Casa saltarplomos(int potencia){
-       Placa potenciaTotal = null;
-       int sumaPotencia = 0;
-       sumaPotencia = sumaPotencia + potencia;
-       if (sumaPotencia > potenciaTotal.getPotencia()){
-           System.out.println("ERROR: Han saltat els ploms. La casa ha quedat completament apagada.");
-           Aparell ElAparell = null;
-           Casa LaCasa = null;
-           LaCasa.offcasa();
-           for (Aparell i : aparells) {
-               if (i.getInterruptor() == true) {
-                   ElAparell.offaparell();
-               }
-           }
-       }
-       return null;
-    }
-    public Casa restaTeulada(int superficie){
-        Placa superficiePlaca = null;
-        Casa superficieTeulada = null;
-        int restantTeulada = superficieTeulada.getSuperficie();
-        restantTeulada = restantTeulada - superficiePlaca.getSuperficie();
-        if (superficiePlaca.getSuperficie() > restantTeulada){
-            System.out.println("ERROR: No hi ha espai disponible per a instal·lar aquesta placa.");
         }
         return null;
     }
-    public Casa potenciaTotal(){
+//Funcion la cual compara si el consumo total de todos los aparatos es mayor a la potencia total de las placas.
+    public Casa saltarplomos(int potencia) {
+        consumTotal();
+        potenciaTotal();
+        if (consumTotal() > potenciaTotal()) {
+            System.out.println("ERROR: Han saltat els ploms. La casa ha quedat completament apagada.");
+            offcasa();
+            for (Aparell i : aparells) {
+                if (i.getInterruptor() == true) {
+                    i.offaparell();
+                }
+            }
+        }
+        return null;
+    }
+//Funcion la cual calcula lo que queda de la teulada de la casa restandole la superficie de la placa.
+    public int restaTeulada() {
+        int restantTeulada = superficie;
+        for (Placa i : placas) {
+            restantTeulada = restantTeulada - i.getSuperficie();
+        }
+        return restantTeulada;
+    }
+//Funcion la cual calcula la potencia total que dan todas las placas instaladas de la casa.
+    public int potenciaTotal() {
         int PotenciaTotal = 0;
         for (Placa i : placas) {
-        PotenciaTotal = PotenciaTotal + i.getPotencia();
-    } return null;
+            PotenciaTotal = PotenciaTotal + i.getPotencia();
+        }
+        return PotenciaTotal;
     }
-    
-    public Casa inversioTotal() {
+//Funcion la cual calcula el precio total que cuestan todas las placas instaladas en la casa.
+    public double inversioTotal() {
         double InversioTotal = 0;
-        for (Placa i : placas){
+        for (Placa i : placas) {
             InversioTotal = InversioTotal + i.getPrecio();
-        } return null;
+        }
+        return InversioTotal;
     }
-    
-    public Casa consumTotal() {
+//Funcion la cual calcula el consumo total de todos los aparatos encendidos en la casa.
+    public int consumTotal() {
         int ConsumTotal = 0;
         for (Aparell i : aparells) {
-            ConsumTotal = ConsumTotal + i.getPotencia();
-        } return null;
+            if (i.getInterruptor() == true) {
+                ConsumTotal = ConsumTotal + i.getPotencia();
+            }
+        }
+        return ConsumTotal;
     }
-    
+
 //    int potencia = datosPlaca.getPotencia();
 //    int superficiePlaca = datosPlaca.getSuperficie();
 //    double precio = datosPlaca.getPrecio();
-
 //    public void addPlaca(String NIF, int superficie, double precio, int potencia) {
 //        if (superficie < 0) {
 //            System.out.println("Error, la superficie tiene que ser mas grande de 0");
